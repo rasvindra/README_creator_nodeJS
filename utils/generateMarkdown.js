16fef53a8af703306ddf8e52e09701279aaed25c
Ravi Nagi <rasvindra@hotmail.com>
2022-11-25 13:50:47 -0800

    created license badge render function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,19 @@
 function createREADME(ans) {
     return`
     
-    
+
 
 
     `;
+}
+
+function createLicense(projLicense){
+    const licensePic = {
+        MIT:`[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`,
+        MOZILLA:`[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`,
+        ISC:`[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)`,
+        IBM:`[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)`,
+        APACHE:`[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`,
+    }
+    return licensePic[projLicense]
 }
